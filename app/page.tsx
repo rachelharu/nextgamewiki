01294fd4eadfc27b8adf95c8f7b970120f816174
rachelharu <rachel.haru9@gmail.com>
2025-04-09 00:04:16 -0700

    set up bulma, sass, and fontawesome

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,61 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <section className="hero is-fullheight is-default is-bold">
+      <div className="hero-head">
+        <nav className="navbar">
+          <div className="container" id="nav">
+            <div className="navbar-brand">
+              <a className="navbar-item" href="../">
+                {/* <Image id="logo" src="/images/game-control.png" alt="Logo" width={100} height={100} /> */}
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <div className="columns is-vcentered">
+            <div className="column is-6 is-offset-3">
+              <div className="wrapper">
+                <h1 className="title is-2 animation">game lookup</h1>
+                <h2 className="subtitle is-4 animationSecond">
+                  find info about video games
+                </h2>
+              </div>
+              <div className="container showBox">
+                <div className="autocomplete" id="autoCom"></div>
+                <div id="summary"></div>
+              </div>
+              {/* <div className="column is-4by4 is-hidden">
+                <figure className="image is-1by1">
+                  <Image
+                    src="https://picsum.photos/800/600/?random"
+                    alt="Description"
+                    width={100} height={100}
+                  />
+                </figure>
+              </div> */}
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+
+      <div className="hero-foot">
+        <div className="container">
+          <div className="tabs is-centered">
+            <ul>
+              <li>
+                <a className="footer-tag" href="https://rawg.io/" target="_blank"
+                  >powered by RAWG</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
